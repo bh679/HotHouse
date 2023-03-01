15dc14a0ae8d8be6dd0764b08f57ad1027ad4dc6
Brennan Hatton <brennan@brennanhatton.com>
2023-03-01 16:04:31 +1100

    Puzzle Cube

---
+++ Assets/Hot House/SnapzoneLockGameObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using BNG;
+
+public class SnapzoneLockGameObject : MonoBehaviour
+{
+	public Grabbable target;
+	public UnityEvent onCorrect, onIncorrect;
+	public SnapZone snapZone;
+	
+	void Reset()
+	{
+		snapZone = this.GetComponent<SnapZone>();
+	}
+	
+	void Start()
+	{
+		snapZone.OnSnapEvent.AddListener(CheckIfCorrectGameObject);
+	}
+	
+	public void CheckIfCorrectGameObject(Grabbable grabbable)
+	{
+		if(grabbable == target)
+		{
+			snapZone.CanRemoveItem = false;
+			snapZone.CanDropItem = false;
+			snapZone.CanSwapItem = false;
+			onCorrect.Invoke();
+		}else
+			onIncorrect.Invoke();
+	}
+}
