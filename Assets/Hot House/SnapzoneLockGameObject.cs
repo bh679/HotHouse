d37ade76c58096e5a435ff6642469aa7b62ab214
Brennan Hatton <brennan@brennanhatton.com>
2023-03-01 16:30:37 +1100

    Puzzle 1 working, with prefab and unity event to call next puzzle

--- Assets/Hot House/SnapzoneLockGameObject.cs
+++ Assets/Hot House/SnapzoneLockGameObject.cs
@@ -9,6 +9,8 @@ public class SnapzoneLockGameObject : MonoBehaviour
 	public Grabbable target;
 	public UnityEvent onCorrect, onIncorrect;
 	public SnapZone snapZone;
+	bool correct = false;
+	public bool Correct{get{return correct;}}
 	
 	void Reset()
 	{
@@ -27,6 +29,7 @@ public class SnapzoneLockGameObject : MonoBehaviour
 			snapZone.CanRemoveItem = false;
 			snapZone.CanDropItem = false;
 			snapZone.CanSwapItem = false;
+			correct = true;
 			onCorrect.Invoke();
 		}else
 			onIncorrect.Invoke();
