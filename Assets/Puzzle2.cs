4c984b15d2b6650d7a89627bb2bbccee330d888b
Brennan Hatton <brennan@brennanhatton.com>
2023-03-08 17:05:59 +1100

    Puzzle 2, commit part 1

---
+++ Assets/Puzzle2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using BNG;
+
+public class Puzzle2 : MonoBehaviour
+{
+	public Lever level;
+	public UnityEvent onComplete;
+	bool leverDown;
+	
+	public void Start()
+	{
+		level.onLeverDown.AddListener(()=>{leverDown = true;});
+		level.onLeverUp.AddListener(()=>{leverDown = false;});
+	}
+	
+	public void CheckLever()
+	{
+		if(leverDown)
+			onComplete.Invoke();
+	}
+}
