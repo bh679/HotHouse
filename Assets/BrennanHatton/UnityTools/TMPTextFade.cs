9bdc918a3fa03b23726c0de46d07bce90ffed616
Brennan Hatton <brennan@brennanhatton.com>
2023-05-03 15:52:37 +0800

    Credits scene

---
+++ Assets/BrennanHatton/UnityTools/TMPTextFade.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace BrennanHatton.UnityTools.LegacyUI
+{
+
+	public class TMPTextFade : MonoBehaviour
+	{
+		public TMP_Text text;
+		
+		Color textColor;
+		
+		void Reset()
+		{
+			text= this.GetComponent<TMP_Text>();
+		}
+		
+		void Start()
+		{
+			textColor = text.color;
+		}
+		
+		public void StartAppear(float time)
+		{
+			if(text.isActiveAndEnabled)
+				StartCoroutine(Appear(time));
+		}
+		
+		IEnumerator Appear(float time)
+		{
+			for(int i = 0; i < time*10; i++)
+			{
+				yield return new WaitForSeconds(0.1f);
+				Debug.Log(i/(time*10) + " " + text.color.maxColorComponent);
+				text.color = new Color(textColor.r,textColor.g,textColor.b,i/(time*10));
+			}
+			text.color = new Color(textColor.r,textColor.g,textColor.b,255);
+		}
+		
+		public void StartDisappear(float time)
+		{
+			if(text.isActiveAndEnabled)
+				StartCoroutine(Disappear(time));
+		}
+		
+		IEnumerator Disappear(float time)
+		{
+			for(int i = 0; i < time*10; i++)
+			{
+				yield return new WaitForSeconds(0.1f);
+				
+				text.color = new Color(textColor.r,textColor.g,textColor.b,1-i/(time*10));
+			}
+			text.color = new Color(textColor.r,textColor.g,textColor.b,0);
+		}
+	}
+
+}
---
+++ Assets/BrennanHatton/UnityTools/TextParticleEffect.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class TextParticleEffect : MonoBehaviour
+{
+	public string textToDisplay;
+	public float createTime;
+	public float hoverTime;
+	public float disassembleTime;
+
+	private ParticleSystem _particleSystem;
+	private TMP_Text _textMeshPro;
+
+	private void Start()
+	{
+		_particleSystem = GetComponent<ParticleSystem>();
+		_textMeshPro = GetComponentInChildren<TMP_Text>();
+
+		StartCoroutine(PlayTextEffect());
+	}
+
+	private IEnumerator PlayTextEffect()
+	{
+		// Set up text
+		_textMeshPro.text = textToDisplay;
+		_textMeshPro.ForceMeshUpdate();
+
+		// Create the particle effect
+		/*var main = _particleSystem.main;
+		main.duration = createTime;
+		_particleSystem.Play();*/
+
+		// Wait for the create time
+		yield return new WaitForSeconds(createTime);
+
+		// Hover the text
+		_textMeshPro.enabled = true;
+		yield return new WaitForSeconds(hoverTime);
+
+		// Disassemble the text
+		_textMeshPro.enabled = false;
+		//_particleSystem. = new ParticleSystem.MinMaxCurve(0, disassembleTime);
+		_particleSystem.Emit(_textMeshPro.textInfo.characterCount);
+
+		// Wait for the disassemble time and stop the particle system
+		yield return new WaitForSeconds(disassembleTime);
+		_particleSystem.Stop();
+	}
+}
